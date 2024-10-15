a79e956eafe096434a90053a4ebf086c753ed8db
Elliott-Roach <elliott.roach@mths.ca>
2024-10-15 11:43:26 -0400

    add neopixle ending

--- main.ts
+++ main.ts
@@ -7,6 +7,7 @@
 
 let lightIntensity = 0
 let neopixleStrip: neopixel.Strip = null
+
 //clean
 basic.clearScreen()
 neopixleStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
@@ -14,6 +15,7 @@ neopixleStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
 neopixleStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
 neopixleStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
 neopixleStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixleStrip.show()
 basic.showIcon(IconNames.Happy)
 
 input.onButtonPressed(Button.A, function () {
@@ -27,42 +29,47 @@ input.onButtonPressed(Button.A, function () {
         neopixleStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
         neopixleStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
         neopixleStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        neopixleStrip.show()
     }
 
 //dark
     if (lightIntensity > 52) {
         neopixleStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
-        neopixleStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.White))
+        neopixleStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
         neopixleStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
         neopixleStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
         neopixleStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        neopixleStrip.show()
     }
 
 //moderit light
     if (lightIntensity > 104) {
         neopixleStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
-        neopixleStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.White))
-        neopixleStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.White))
+        neopixleStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
+        neopixleStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
         neopixleStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
         neopixleStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        neopixleStrip.show()
     }
 
 //bright
     if (lightIntensity > 156) {
         neopixleStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
-        neopixleStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.White))
-        neopixleStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.White))
-        neopixleStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.White))
+        neopixleStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
+        neopixleStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
+        neopixleStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Blue))
         neopixleStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        neopixleStrip.show()
     }
 
 //vary light
     if (lightIntensity > 208) {
         neopixleStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
-        neopixleStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.White))
-        neopixleStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.White))
-        neopixleStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.White))
-        neopixleStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.White))
+        neopixleStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
+        neopixleStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
+        neopixleStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Blue))
+        neopixleStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Blue))
+        neopixleStrip.show()
     }
 
 })
